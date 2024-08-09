fc42e2010da42e7510fcf6f889e3b75fcd99ebd7
joseph.xiang <joseph.xiang@evg.co>
2024-08-09 17:13:25 +0800

    feat: init

---
+++ spring-grpc/src/test/java/com/xj/springgrpc/SpringGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xj.springgrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
